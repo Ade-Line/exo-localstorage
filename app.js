f59d7b5ab9244431d1c65d12f7fc64b8c0488332
Ade-Line <adelinecouture@gmail.com>
2020-03-02 19:39:52 +0100

    fin exo

--- app.js
+++ app.js
@@ -2,7 +2,23 @@
 
 // Créer l'évènement save()
 
-function save() {
-    let tache = document.getElementById('tache').nodeValue
-    localStorage.setItem('nameTask', tache)
-}
+// function save() {
+//     let tache = document.getElementById('tache').value;
+//     localStorage.setItem('nameTask', tache)
+// }
+
+// // Créer l'évènement remove()
+// function remove() {
+//     document.getElementById('tache').value = 'Le chef il veut du à l ail';
+//     localStorage.removeItem('nameTask')
+// }
+
+const lienGoogle = document.getElementById('google')
+const cg = document.getElementById('cg')
+
+lienGoogle.addEventListener('click', function(e){
+    if(!cg.checked){
+        e.preventDefault()
+    }
+
+})
